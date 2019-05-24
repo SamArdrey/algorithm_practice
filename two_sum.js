972cb8ab7f4797370be9d67cada53f0bebc5b552
Sam Ardrey <samardrey@gmail.com>
2019-05-23 21:17:19 -0700

    changes

--- two_sum.js
+++ two_sum.js
@@ -7,8 +7,8 @@ var twoSum = function(nums, target) {
       }
   }
 };
-
+ 
 // Runtime: 112 ms, faster than 51.27% of JavaScript
 // online submissions for Two Sum.
 // Memory Usage: 34.9 MB, less than 43.90% of JavaScript
-// online submissions for Two Sum.  
+// online submissions for Two Sum.
